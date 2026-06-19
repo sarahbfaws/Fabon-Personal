e2aef0ba73560478c020fe4906711d8293537972
sarahbfaws <sarahbf@amazon.com>
2026-06-19 11:33:25 +1000

    Updating website with new design and new project section

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Select the elements
-    const toggle_btn = document.querySelector(".toggle-btn");
-    const big_wrapper = document.querySelector(".big-wrapper");
-    const hamburger_menu = document.querySelector(".hamburger-menu");
+document.addEventListener("DOMContentLoaded", () => {
+  // Select the elements
+  const toggle_btn = document.querySelector(".toggle-btn");
+  const big_wrapper = document.querySelector(".big-wrapper");
+  const hamburger_menu = document.querySelector(".hamburger-menu");
 
-    // Ensure the elements exist before adding event listeners
-    if (toggle_btn) {
-        toggle_btn.addEventListener("click", () => {
-            big_wrapper.classList.toggle("dark");
-        });
-    }
-
-    if (hamburger_menu) {
-        hamburger_menu.addEventListener("click", () => {
-            big_wrapper.classList.toggle("active");
-        });
-    }
-
-    
-
-    // Initialize Typed.js
-    new Typed('#intro', {
-        strings: ['Cloud Support Associate', 'Web Developer', 'Creative'],
-        typeSpeed: 50,
-        backSpeed: 25,
-        loop: true, // Adds looping effect
+  // Ensure the elements exist before adding event listeners
+  if (toggle_btn) {
+    toggle_btn.addEventListener("click", () => {
+      big_wrapper.classList.toggle("dark");
     });
+  }
 
-    // Initialize AOS
-    AOS.init({
-        once: true, // Animation happens only once and stays
+  if (hamburger_menu) {
+    hamburger_menu.addEventListener("click", () => {
+      big_wrapper.classList.toggle("active");
     });
+  }
+
+  // Initialize Typed.js
+  new Typed("#intro", {
+    strings: ["Web Developer", "Creative", "Cloud Enthusiast"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    loop: true, // Adds looping effect
   });
+
+  // Initialize AOS
+  AOS.init({
+    once: true, // Animation happens only once and stays
+  });
+});
